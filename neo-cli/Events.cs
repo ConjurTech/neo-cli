b2fc7f731166400684d99944f49fb875c91e905c
spclight <spclight@gmail.com>
2018-01-16 01:18:00 +0800

    Successfully connect to local database and scaffold. Havent test read/write yet

---
+++ neo-cli/Events.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Neo
+{
+    public partial class Events
+    {
+        public string Type { get; set; }
+        public string Data { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ neo-cli/neocliContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Neo
+{
+    public partial class neocliContext : DbContext
+    {
+        public virtual DbSet<Events> Events { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql(@"Host=localhost;Database=neocli;Username=postgres;Password=postgres");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Events>(entity =>
+            {
+                entity.ToTable("events");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Data).HasColumnName("data");
+
+                entity.Property(e => e.Type).HasColumnName("type");
+            });
+        }
+    }
+}
